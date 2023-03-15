258587071b18804357ac1ec454af407a26f9cab0
JailsonA <maurocv18@gmail.com>
2023-03-15 17:46:38 +0000

    Data Base connection done

--- eTickets/Startup.cs
+++ eTickets/Startup.cs
@@ -1,13 +1,22 @@
 ﻿using eTickets.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace eTickets
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         public void configureServices(IServiceCollection services) {
             
             // DbContext Config
-            services.AddDbContext<AppDbContext>();
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString")));
         }
     }
 }
